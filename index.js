45de90951c34fd757476e10094430ed76513342e
Rayhatron <rayhatron@gmail.com>
2017-05-26 13:11:41 +0200

    Deployment test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.json(req.headers);
 });
 
 app.listen(PORT, () => {
